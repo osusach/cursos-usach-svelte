217473662903295749ed5a45b56f1ecc8abc4a7a
Skyrdow <lucas.mesias@usach.cl>
2024-09-01 01:40:30 -0400

    add: auth buttons

--- src/lib/components/SignIn.svelte
+++ src/lib/components/SignIn.svelte
@@ -3,17 +3,24 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>
+<button
+	class="btn btn-secondary col-span-2"
+	on:click={() => {
+		if ($page.data.session) {
+			signOut();
+		} else {
+		}
+		signIn('google');
+	}}
+>
 	{#if $page.data.session}
 		<span>
 			<small>Signed in as</small><br />
 			<strong>{$page.data.session.user?.name ?? 'User'}</strong>
 		</span>
-		<button on:click={() => signOut()} class="button">Sign out</button>
+		<span>Sign out</span>
 	{:else}
 		<span>You are not signed in</span>
-		<button on:click={() => signIn('google')}> Sign In with Google </button>
+		<span> Sign In with Google </span><span class="iconify mingcute--google-fill size-6"></span>
 	{/if}
-</p>
+</button>

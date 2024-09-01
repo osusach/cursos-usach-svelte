e409d4e4f098a46d487c104aba0735c1057e9afc
Skyrdow <lucas.mesias@usach.cl>
2024-09-01 01:24:29 -0400

    fix: trustHost

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,5 +3,6 @@ import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 import Google from '@auth/core/providers/google';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
+	trustHost: true,
 	providers: [Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })]
 });
